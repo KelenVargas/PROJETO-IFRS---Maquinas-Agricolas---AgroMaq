86761a035a023a7de929fd36113f368e15e76802
Kelen Vargas <kelenvargas.senac@gmail.com>
2025-12-17 10:16:58 -0300

    adicionando arquivo app.py com configuração inicial

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask
 #import do banco de dados
+from conf.database import init_db
 
+# módulos 
+from control.index import index_bp
 
 app = Flask(__name__)
 
 
+#Conexao Geral do meu app
+init_db(app)
 
-#Registro de controladores 
+#Registro de controladores
+app.register_blueprint(index_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
