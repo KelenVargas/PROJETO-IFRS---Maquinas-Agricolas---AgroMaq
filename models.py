ecf6c6162b96345ebdf11d99c60e4bf58a1e93c2
Kelen Vargas <kelenvargas.senac@gmail.com>
2026-02-20 08:58:49 -0300

    incluindo as classes

--- models.py
+++ models.py
@@ -17,3 +17,31 @@ class ItensProposta(db.Model):
     id_produto = db.Column(db.Integer, db.ForeignKey('produtos.id_produto'), nullable=False)
     quantidade = db.Column(db.Integer, nullable=False)
     preco_unitario = db.Column(db.Numeric(12,2), nullable=False)
+    
+class Proposta(db.Model):
+    __tablename__ = 'propostas'
+    id_proposta = db.Column(db.Integer, primary_key=True)
+    id_produtor = db.Column(db.Integer, db.ForeignKey('produtor.id_produtor'), nullable=False)  
+    data_proposta = db.Column(db.Date)
+    status = db.Column(db.String(50), default='aberta')
+    itens = db.relationship('ItensProposta', backref='proposta', lazy=True)
+    
+class Produtor(db.Model):
+    __tablename__ = 'produtor'
+    id_produtor = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    cpf = db.Column(db.String(11), unique=True)
+    cnpj = db.Column(db.String(14), unique=True)
+    telefone = db.Column(db.String(20))
+    email = db.Column(db.String(100))
+    tipo_cultura = db.Column(db.String(50))
+    tamanho_area = db.Column(db.Numeric(10,2))
+    endereco = db.Column(db.String(200))
+    score = db.Column(db.Integer, default=50) 
+
+class Usuarios(db.Model):
+    __tablename__ = 'usuarios'
+    id = db.Column(db.Integer, primary_key=True)
+    usuario = db.Column(db.String(50), unique=True, nullable=False)
+    senha = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True)
