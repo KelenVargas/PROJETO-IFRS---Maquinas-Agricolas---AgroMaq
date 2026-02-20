ea978323e34e9598f8e51839d4dc7c42fb6addde
Kelen Vargas <kelenvargas.senac@gmail.com>
2026-02-20 08:59:50 -0300

    ajustando o env. no database

--- conf/database.py
+++ conf/database.py
@@ -1,9 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
 
+load_dotenv()# Carrega as variáveis do .env
 
 db = SQLAlchemy()
 
 def init_db(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:123@localhost/Agromaq'
-
+    # Puxa a URL completa do banco do arquivo .env
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URL')
     db.init_app(app)
