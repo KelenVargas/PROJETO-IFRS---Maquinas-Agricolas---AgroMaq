143adc1fa7f9aad76d85bb849ceba72ae26e9a5b
Kelen Vargas <kelenvargas.senac@gmail.com>
2026-02-26 10:53:10 -0300

    ajustando o .env

--- conf/database.py
+++ conf/database.py
@@ -2,11 +2,11 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from dotenv import load_dotenv
 
-load_dotenv()# Carrega as variáveis do .env
+load_dotenv() # Carrega as variáveis do .env
 
 db = SQLAlchemy()
 
 def init_db(app):
-    # Puxa a URL completa do banco do arquivo .env
+    # Puxa a URL do banco de forma protegida
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URL')
     db.init_app(app)
